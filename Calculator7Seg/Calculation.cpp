85bff089dd681aef0eab3b3a8a9776feb8b2dcac
Michel Mirsayyah <michel.mirsayyah@ost.ch>
2020-09-15 09:37:41 +0200

    added Calculation

---
+++ Calculator7Seg/Calculation.cpp
@@ -0,0 +1,16 @@
+#include "Calculation.h"
+
+int Calc(int a, int b, char operation)
+{
+	switch (operation)
+	{
+	case '+':
+		return a + b;
+	case '-':
+		return a - b;
+	case '*':
+		return a * b;
+	case '/':
+		return a / b;
+	}
+}
